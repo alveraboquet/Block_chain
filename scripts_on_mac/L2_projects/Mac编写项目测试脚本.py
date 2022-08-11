02a91fe924c34862d980d129d1d797df7f68a25b
spencer <1570561804@qq.com>
2022-08-11 21:34:24 +0800

    mac

--- scripts_on_mac/L2_projects/Mac编写项目测试脚本.py
+++ scripts_on_mac/L2_projects/Mac编写项目测试脚本.py
@@ -1,6 +1,9 @@
 #pycharm通过git同步代码：窗口右上角粉色小箭头➡️
 from functions import *
 
+import linecache
+
+
 excel_path = '/Users/spencer/PycharmProjects/Block_chain/eth1000_操作后.xlsx'
 write_success_to_excel_column = "D"  #把成功或失败记录到excel的列
 read_from_excel_column = "D" #从excel中的哪一列读取状态? 判断是不是要做任务?
@@ -8,41 +11,39 @@ excel_start_row = 30
 browser_wait_times = 10
 
 for i in range(excel_start_row, 201):
-    # one_sign =str(Do_Excel(excel_path).read(i, read_from_excel_column))
-    # if "1" in one_sign:
-    fail_sign = Do_Excel(excel_path).read(i, read_from_excel_column)
+    print(f"第 {i} 个号需要做zk")
+    ##=========== 准备浏览器、切换IP、清理缓存
+    wait, browser = my_mac_chrome(time_out = browser_wait_times)
+    # wait, browser = my_linux_chrome(time_out=browser_wait_times)
+    # open_clash_dashboard(browser, wait, url_dashboard)
+    # random_select_clash_ip(browser, wait)
+    # delete_cookie(browser)
 
-    if fail_sign != "成功":
-        print(f"第 {i} 个号需要做zk")
-        ##=========== 准备浏览器、切换IP、清理缓存
-        wait, browser = my_mac_chrome(time_out = browser_wait_times)
+    ##=========== 登陆小狐狸，
+    login_metamask(browser, wait, metamask_pw, metamask_home)
 
-        # open_clash_dashboard(browser, wait, url_dashboard)
-        # random_select_clash_ip(browser, wait)
-        # delete_cookie(browser)
+    # switch_tab_by_handle(browser, 1, 0)  # 切换到小狐狸
 
-        ##=========== 登陆小狐狸，
-        login_metamask(browser, wait, metamask_pw, metamask_home, "Optimism")
-
-        # switch_tab_by_handle(browser, 1, 0)  # 切换到小狐狸
-
-        # 小狐狸换号
-        print(f"===============小狐狸开始换号{i} ==============")
-        fox_change_account(browser, wait, i)  #换号，选列表里的
+    # =============小狐狸换号
+    print(f"===============小狐狸开始换号{i} ==============")
+    fox_change_account(browser, wait, i)  #换号，选列表里的
 
 
-        ##=========== 开始做任务
-        save_record = galaxy_claim_op_hop_NFT(browser, wait)
-        print("记录是：",save_record)
+    ##=========== 开始做任务
+    CID_text = linecache.getline(r"json_CID.txt", 2)
+    print("这次用的CID_text是",CID_text)
+    save_record = zksync_mint_NFT(browser, wait, CID_text)
 
-        if "成功" in save_record:
-            Do_Excel(excel_path, sheetname='SheetJS').plain_write(i, write_success_to_excel_column, "成功")
-        else:
-            Do_Excel(excel_path, sheetname='SheetJS').plain_write(i, write_success_to_excel_column, "×")
+    print("记录是：",save_record)
 
-        a = random.randint(10, 15)
-        time_sleep(a, f"++++++++++随机等待时间{a}")
-        browser.quit()
-        a = random.randint(10, 15)
-        time_sleep(a, f"++++++++++随机等待时间{a}")
+    # if "成功" in save_record:
+    #     Do_Excel(excel_path, sheetname='SheetJS').plain_write(i, write_success_to_excel_column, "成功")
+    # else:
+    #     Do_Excel(excel_path, sheetname='SheetJS').plain_write(i, write_success_to_excel_column, "×")
+
+    a = random.randint(10, 15)
+    time_sleep(a, f"++++++++++随机等待时间{a}")
+    browser.quit()
+    a = random.randint(10, 15)
+    time_sleep(a, f"++++++++++随机等待时间{a}")
 
