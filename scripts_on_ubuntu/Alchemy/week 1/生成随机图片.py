a80333909d27e5ceab73e0fc676957cdb5b1434c
LittleTerry <1570561804@qq.com>
2022-08-25 01:18:29 +0800

    al

--- scripts_on_ubuntu/Alchemy/week 1/生成随机图片.py
+++ scripts_on_ubuntu/Alchemy/week 1/生成随机图片.py
@@ -1,8 +1,12 @@
 from randimage import get_random_image, show_array
+from alive_progress import alive_bar
+
 import matplotlib
 img_size = (350,350)
-for i in range(1, 141):
-    img = get_random_image(img_size)  #returns numpy array
-    # show_array(img) #shows the image
-    savepath = './photos/'
-    matplotlib.image.imsave( savepath +f"{i}.png", img)
+
+with alive_bar(26, dual_line=True, title='Alphabet') as bar:
+    for i in range(1, 150):
+        img = get_random_image(img_size)  #returns numpy array
+        # show_array(img) #shows the image
+        savepath = '/home/parallels/ubuntu_zk/Block_chain/scripts_on_ubuntu/Alchemy/week 1/photos/'
+        matplotlib.image.imsave( savepath +f"{i}.png", img)
